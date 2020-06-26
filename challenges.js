25bbac91a98f228dc476831903b9822f6e59f12d
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-06-26 14:48:54 +0200

    implementing challenge 2 solution

--- challenges.js
+++ challenges.js
@@ -44,5 +44,47 @@ let solution1 = (arr) => {
     return arr;
 }
 
-exports.solveChallenge1 = solution1;
+/*
+input eg:
+[
+    [0  , e01, e02]
+    [e10,   0,   0]
+    [0  ,   0, e22]
+]
+*/
 
+/*
+output eg:
+[
+    [e00, e01, e02]
+    [e10, e11, e12]
+    [e20, e21, e22]
+]
+*/
+
+let solution2 = (arr) => {
+    let e01 = arr[0][1];
+    let e02 = arr[0][2];
+    let e10 = arr[1][0];
+    let e22 = arr[2][2];
+
+    let e11 = e01 + e02 - e22;
+    let e21 = e11 + e02 - e22;
+    let sum = e01 + e11 + e21;
+
+    let e00 = sum - (e01 + e02);
+    let e20 = sum - (e00 + e10);
+    let e12 = sum - (e10 + e11);
+
+    arr[0][0] = e00;
+    arr[1][1] = e11;
+    arr[1][2] = e12;
+    arr[2][0] = e20;
+    arr[2][1] = e21;
+
+    return arr;
+}
+
+
+exports.solveChallenge1 = solution1;
+exports.solveChallenge2 = solution2;
